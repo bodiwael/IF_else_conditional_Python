d035c521ad47778d1a8d4a2e674c23b59bafa9f0
Abdelrahman Wael Abdelrahman Rashad Ammar <73370043+bodiwael@users.noreply.github.com>
2022-08-11 23:31:50 +0200

    Add files via upload

---
+++ If Law.py
@@ -0,0 +1,78 @@
+
+X = 3
+
+if X == 5 :
+    
+    print(X)
+    
+    print(X*3)
+
+print(5*X)
+
+
+
+#-----------------------------------------------------------------------------
+
+Y = 20
+
+if Y > 50 :
+    
+    print("Hello World")
+    
+else:
+    
+    print("Bye World")
+    
+    
+#----------------------------------------------------------------------------
+
+
+a = 8
+
+if a > 15 :
+    
+    print( "a is more than 15" )
+    
+elif a < 5 :
+    
+    print( "a is less than 5" )
+    
+elif a > 5 :
+    
+    print( "a is more than 5" )
+    
+else :
+    
+    print( "a is less than 15" )
+    
+#----------------------------------------------------------------------------
+
+a = 5 
+
+b = 7
+
+if ( a == 5 ) or ( b == 2 ) :
+    
+    print("Hello World !")
+    
+if ( a == 5 ) and ( b == 2 ) :
+    
+    print("Hello World !")
+    
+else :
+    
+    print("Bey World")
+    
+if ( a == 5 ) and ( b == 7 ) :
+    
+    print("Hello World !")
+    
+#-------------------------------------------------------------------------------
+
+a = 7
+
+b = 5 
+
+maximum = a if a > b else b
+
+print(maximum)
